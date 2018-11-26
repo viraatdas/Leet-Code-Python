85294b29b844af76cbbbf125fd058f99569a4b58
Viraat Das <viraat.laldas@gmail.com>
2018-11-26 14:07:43 -0500

    812ambiguous - need to add for decimal points for x and y

--- Strings/812ambiguousCoordinates.py
+++ Strings/812ambiguousCoordinates.py
@@ -7,6 +7,7 @@ def ambiguousCoordinates(S):
     output = []
     S = S[1: -1]
     i = 0
+    print (S)
     while i < len(S) - 1:
         if S[:i + 1] == str(int(S[:i + 1])) and S[i + 1] == str(int(S[i + 1:])):
             output.append(("({}, {})").format(S[:i + 1], S[i + 1:]))
@@ -15,13 +16,13 @@ def ambiguousCoordinates(S):
     i = 0
     while i < len(S) - 1:
         j = i + 1
-        while j < len(S) - 1: #for decimal point on the x coordinate
-            if S[:i +  1] == str(int(S[:i +  1])) and S[j + 1:] == str(int(S[j + 1:])):
+        while j < len(S) - 1: #for decimal point on the x coordinate only
+            if S[:i +  1] == str(int(S[:i +  1])) and S[i + 1:j + 1][-1] != "0" and S[j + 1:] == str(int(S[j + 1:])):
                 output.append(("({}.{}, {})").format(S[:i +  1], S[i + 1:j + 1], S[j + 1:]))
             j += 1
         p = i + 1
-        while p < len(S) - 1: #for decimal on the y coordinate
-            if S[:i +  1] == str(int(S[:i +  1])) and S[p + 1:] == str(int(S[p + 1:])):
+        while p < len(S) - 1: #for decimal on the y coordinate only
+            if S[:i +  1] == str(int(S[:i +  1])) and S[i + 1: p + 1] == str(int(S[i + 1: p + 1])):
                 output.append(("({}, {}.{})").format(S[:i +  1], S[i + 1: p + 1], S[p + 1:]))
             p += 1
         i += 1
